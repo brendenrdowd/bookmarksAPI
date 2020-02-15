341675a740019a44787853ae9a194a658f61ab9e
Daniel Di Venere <danieldivenere@gmail.com>
2020-02-15 12:35:49 -0800

    everything is working

---
+++ seeds/seed.blogful_articles.sql
@@ -0,0 +1,8 @@
+insert into bookmarks (title, description, url, rating) 
+  values ('Google', 'Exercitation est laborum enim irure ipsum eu.', 'google.com', 2),
+  ('Bing', 'Irure magna laborum ullamco esse elit dolor mollit tempor duis tempor incididunt occaecat cillum.', 'bing.com', 1),
+  ('Yahoo', 'Ea exercitation mollit excepteur reprehenderit enim irure aliquip ad.', 'yahoo.com', 1),
+  ('DuckDuckGo', 'Veniam sit do laboris eiusmod magna veniam cupidatat nostrud quis.', 'duckduckgo.com', 4),
+  ('DogPile', 'Occaecat nostrud est dolore non sint elit ullamco id occaecat est sit.', 'dogpile.com', 5),
+  ('Ask.com', 'Qui est velit quis sint elit duis eu qui non sint sit aliqua tempor Lorem.', 'ask.com', 2),
+  ('Search.com', 'Quis quis eiusmod ad qui consequat dolor.', 'search.com', 3); 
---
+++ src/bookmarks/bookmarksService.js
@@ -0,0 +1,19 @@
+const bookmarksService = {
+  getAll(db) {
+    return db.select('*').from('bookmarks')
+  },
+  getById(db, id) {
+    return db.select('*').from('bookmarks').where('id', id).first()
+  },
+  insertBookmark(db, newBookmark) {
+    return db.from('bookmarks').returning('*').insert(newBookmark)
+  },
+  updateBookmark(db, id, newData) {
+    return db('bookmarks').where({ id }).update(newData)
+  },
+  deleteBookmark(db, id) {
+    return db('bookmarks').where({ id }).delete()
+  }
+}
+
+module.exports = bookmarksService;
