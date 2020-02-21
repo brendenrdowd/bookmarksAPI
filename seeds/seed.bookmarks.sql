3dce78aa1fc453af87e2dcef54627707b286c329
Daniel Di Venere <danieldivenere@gmail.com>
2020-02-20 22:56:15 -0800

    changed seeds

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -1,8 +1,8 @@
 insert into bookmarks (title, description, url, rating) 
-  values ('Google', 'Exercitation est laborum enim irure ipsum eu.', 'google.com', 2),
-  ('Bing', 'Irure magna laborum ullamco esse elit dolor mollit tempor duis tempor incididunt occaecat cillum.', 'bing.com', 1),
-  ('Yahoo', 'Ea exercitation mollit excepteur reprehenderit enim irure aliquip ad.', 'yahoo.com', 1),
-  ('DuckDuckGo', 'Veniam sit do laboris eiusmod magna veniam cupidatat nostrud quis.', 'duckduckgo.com', 4),
-  ('DogPile', 'Occaecat nostrud est dolore non sint elit ullamco id occaecat est sit.', 'dogpile.com', 5),
-  ('Ask.com', 'Qui est velit quis sint elit duis eu qui non sint sit aliqua tempor Lorem.', 'ask.com', 2),
-  ('Search.com', 'Quis quis eiusmod ad qui consequat dolor.', 'search.com', 3); 
+  values ('Google', 'Exercitation est laborum enim irure ipsum eu.', 'https://google.com', 2),
+  ('Bing', 'Irure magna laborum ullamco esse elit dolor mollit tempor duis tempor incididunt occaecat cillum.', 'https://bing.com', 1),
+  ('Yahoo', 'Ea exercitation mollit excepteur reprehenderit enim irure aliquip ad.', 'https://yahoo.com', 1),
+  ('DuckDuckGo', 'Veniam sit do laboris eiusmod magna veniam cupidatat nostrud quis.', 'https://duckduckgo.com', 4),
+  ('DogPile', 'Occaecat nostrud est dolore non sint elit ullamco id occaecat est sit.', 'https://dogpile.com', 5),
+  ('Ask.com', 'Qui est velit quis sint elit duis eu qui non sint sit aliqua tempor Lorem.', 'https://ask.com', 2),
+  ('Search.com', 'Quis quis eiusmod ad qui consequat dolor.', 'https://search.com', 3); 
